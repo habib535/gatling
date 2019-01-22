d105522c069221b039ae42ff9023905a00a8dbe4
habib535 <habib535@gmail.com>
2019-01-22 17:47:49 +0100

    dots

--- src/test/scala/sample/HttpSimulation.scala
+++ src/test/scala/sample/HttpSimulation.scala
@@ -2,6 +2,7 @@ package sample
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import io.gatling.commons.validation._
 
 import scala.concurrent.duration._
 
@@ -14,16 +15,16 @@ class HttpSimulation extends Simulation {
     .acceptHeader("application/json")
   //.wsBaseUrl("ws://" + targetAddress)
 
-  var scn = scenario("Http Hello World").
-    exec {
+  var scn = scenario("Http Hello World")
+    .exec {
       session =>
         println(session)
         session
-    }.
-    exec(http("home page").
-      get("/showroom/products"))
+    }
+    .exec(http("home page")
+      .get("/showroom/products"))
     pause(5 seconds)
 
-  setUp(scn.inject(atOnceUsers(1))).
-    protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(1)))
+    .protocols(httpProtocol)
 }
